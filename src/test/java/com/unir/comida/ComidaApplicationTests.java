154f698531a3ebb304fdd68f58c8ef6ee29060ea
Javier Díaz Díaz <jddiazsena@gmail.com>
2024-10-09 21:24:06 -0500

    primer commit

---
+++ src/test/java/com/unir/comida/ComidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.comida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
